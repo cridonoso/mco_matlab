01744e966144ff7c4521f9fdc092febc8fbc7e7e
Cristobal Donoso <cridonoso@inf.udec.cl>
2025-03-20 19:05:07 -0300

    final version + figures

--- H2.m
+++ H2.m
@@ -1,22 +1,23 @@
 clearvars
 clc
-%% Question 1
+%% Question 2
 rng(42); % seed to recall same charts 
 addpath("utils") % add util functions
 
-N = 100; % sample size
+N_vector = [50, 100, 500]; % sample sizes vector
 beta = [1, 2, 1,-1]; % beta coefs.
-sigma_u = [1, 2, 10]; % variance for U
+sigma_e = 2; % variance for U
 
 % Initialize estimation
 n_iter = 500;
-beta_estimates = zeros(length(sigma_u), n_iter, length(beta));
-for j=1:length(sigma_u)
+beta_estimates = zeros(length(N_vector), n_iter, length(beta));
+for j=1:length(N_vector)
     for i=1:n_iter
-    
+        N = N_vector(j); % selecting current N size
+        
         % Defining input matrix 
         X = generate_data(N, [0, 0, 0], [1, 2, 1]);
-        U = sample_normal(N, 0, sigma_u(j));
+        U = sample_normal(N, 0, sigma_e);
         
         % Target
         Y = X*beta' + U;
@@ -31,28 +32,29 @@ for j=1:length(sigma_u)
 end
 
 [beta_estimates, ptiles] = sort_percentile(beta_estimates);
+
 %% PLOT
 % Create tiled layout
 t = tiledlayout(2, 2, 'TileSpacing', 'compact', 'Padding', 'loose');
 
-for j = 1:4  % j=1 corresponde a β0, j=2 a β1, etc.
+% Add subplots
+for j = 1:length(beta)
     nexttile;
     hold on;
-    % Se calcula la densidad de los estimadores para cada valor de sigma² usando myKSDensity
-    for s = 1:length(sigma_u)
+    for s = 1:length(N_vector)
         data = beta_estimates(s, :, j);
         [xi, f] = kde(data);
-        plot(xi, f, 'LineWidth', 1, 'DisplayName', sprintf('\\sigma^2 = %g', sigma_u(s)));
+        plot(xi, f, 'LineWidth', 1, 'DisplayName', sprintf('N = %g', N_vector(s)));
     end
-    % Línea vertical que indica el valor verdadero del parámetro
     xline(beta(j), '--r', 'LineWidth', 1, 'DisplayName', 'True value');
-    ylabel(sprintf('Densidad'));
+    ylabel('Densidad');
     xlabel(sprintf('Valor estimado para \\beta_{%g}', j-1));
     grid on;
     hold off;
 end
+
 % Add shared title
-sgtitle("Distribuci\'{o}n de $\hat{\beta}$ variando $\sigma^2_U$", 'Interpreter', 'latex', 'FontSize', 14);
+sgtitle("Distribuci\'{o}n de $\hat{\beta}$ variando $N$", 'Interpreter', 'latex', 'FontSize', 14);
 
 % Add legend below the plots
 hLegend = legend('show', 'NumColumns', 2);
@@ -62,4 +64,5 @@ axis padded
 set(gcf, 'PaperUnits', 'inches');
 set(gcf, 'PaperSize', [6, 5]); % Set paper size larger than figure
 set(gcf, 'PaperPosition', [0., 0., 6, 5]); % Add 
-print(gcf, '-dpdf', './figures/output_1.pdf');
+print(gcf, '-dpdf', './figures/output_2.pdf');
+
--- H3.m
+++ H3.m
@@ -1,29 +1,33 @@
 clearvars
 clc
-%% Question 1
+
+%% Question 3
 rng(42); % seed to recall same charts 
 addpath("utils") % add util functions
 
-N = 100; % sample size
+N = 100; % sample sizes vector
 beta = [1, 2, 1,-1]; % beta coefs.
-sigma_u = [1, 2, 10]; % variance for U
+sigma_u = 2; % variance for U
+sigma_z = 1; % variance for Z
+lambda_v = [0.1, 0.5, 5]; % lambda values 
 
 % Initialize estimation
 n_iter = 500;
-beta_estimates = zeros(length(sigma_u), n_iter, length(beta));
-for j=1:length(sigma_u)
+beta_estimates = zeros(length(lambda_v), n_iter, length(beta));
+for j=1:length(lambda_v)
     for i=1:n_iter
-    
-        % Defining input matrix 
-        X = generate_data(N, [0, 0, 0], [1, 2, 1]);
-        U = sample_normal(N, 0, sigma_u(j));
         
+        % Defining input matrix 
+        Z   = sample_normal(N, 0, sigma_z);
+        U   = sample_normal(N, 0, sigma_u);
+        X12 = generate_data(N, [0, 0], [1, 2]);
+        X3  = Z + lambda_v(j).*U;
+        X   = [X12 X3];
+
         % Target
         Y = X*beta' + U;
         
         % Beta parameter
-        %beta_hat = inv(X'*X)*X'Y % inneficient
-        %beta_hat = (X'*X) \ (X'*Y); % better but not enough
         beta_hat = X \ Y; % best
     
         beta_estimates(j, i, :) = beta_hat;
@@ -31,28 +35,28 @@ for j=1:length(sigma_u)
 end
 
 [beta_estimates, ptiles] = sort_percentile(beta_estimates);
+
 %% PLOT
 % Create tiled layout
 t = tiledlayout(2, 2, 'TileSpacing', 'compact', 'Padding', 'loose');
 
-for j = 1:4  % j=1 corresponde a β0, j=2 a β1, etc.
+for j = 1:length(beta)  % iterate over beta indices
     nexttile;
     hold on;
-    % Se calcula la densidad de los estimadores para cada valor de sigma² usando myKSDensity
-    for s = 1:length(sigma_u)
+    for s = 1:length(lambda_v) % Iterate over N sizes
         data = beta_estimates(s, :, j);
         [xi, f] = kde(data);
-        plot(xi, f, 'LineWidth', 1, 'DisplayName', sprintf('\\sigma^2 = %g', sigma_u(s)));
+        plot(xi, f, 'LineWidth', 1, 'DisplayName', sprintf('\\lambda = %g', lambda_v(s)));
     end
-    % Línea vertical que indica el valor verdadero del parámetro
     xline(beta(j), '--r', 'LineWidth', 1, 'DisplayName', 'True value');
-    ylabel(sprintf('Densidad'));
+    ylabel('Densidad');
     xlabel(sprintf('Valor estimado para \\beta_{%g}', j-1));
     grid on;
     hold off;
 end
+
 % Add shared title
-sgtitle("Distribuci\'{o}n de $\hat{\beta}$ variando $\sigma^2_U$", 'Interpreter', 'latex', 'FontSize', 14);
+sgtitle("Distribuci\'{o}n de $\hat{\beta}$ variando $\lambda$", 'Interpreter', 'latex', 'FontSize', 14);
 
 % Add legend below the plots
 hLegend = legend('show', 'NumColumns', 2);
@@ -62,4 +66,4 @@ axis padded
 set(gcf, 'PaperUnits', 'inches');
 set(gcf, 'PaperSize', [6, 5]); % Set paper size larger than figure
 set(gcf, 'PaperPosition', [0., 0., 6, 5]); % Add 
-print(gcf, '-dpdf', './figures/output_1.pdf');
+print(gcf, '-dpdf', './figures/output_3.pdf');
